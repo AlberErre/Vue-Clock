4d2fa7d256f6fcb387e3098950e43757e95b7998
Alber Erre <arblugo@gmail.com>
2018-11-15 19:26:17 +0000

    init app

---
+++ src/components/HTMLClock.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="html-clock">
+    <div class="hours">{{ hours }}</div>
+    <div class="sep">:</div>
+    <div class="minutes">{{ minutes }}</div>
+    <div class="mode">:</div>
+    <div class="seconds">{{ seconds }}</div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+  .html-clock {
+    display: grid;
+    grid-template-columns: 3fr 1fr 3fr 1fr 3fr 1.5fr;
+    justify-content: center;
+    align-items: center;
+    width: 500px;
+    height: 125px;
+  }
+  
+  .html-clock > div {
+    display: flex;
+    justify-content: center;
+  }
+  
+  .sep {
+    animation: papasitoBlink 1s linear infinite;
+  }
+  
+  @keyframes papasitoBlink {
+    0%,
+    49% { opacity: 1; }
+    
+    50%,
+    100% { opacity: 0; }
+  }
+  
+  .red-lcd {
+    color: red;
+  }
+  
+  .green-lcd {
+    color: green;
+  }
+  
+  .yellow-lcd {
+    color: yellow;
+  }
+</style>
+
+
